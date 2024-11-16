cd210295e99de8a3ff92781783c6fadfb952afbf
cruvie <cruvie@>
2024-11-17 01:07:16 +0800

    rebuild api gen
    Signed-off-by: cruvie <cruvie@>

--- internal/api_etcd/generate_api_test.go
+++ internal/api_etcd/generate_api_test.go
@@ -1,64 +1,64 @@
 package api_etcd
 
 import (
-	"gitee.com/cruvie/kk_go_kit/kk_http"
+	"gitee.com/cruvie/kk_go_kit/kk_http/kk_api_gen"
 	"testing"
 )
 
 func TestUser(t *testing.T) {
-	apiGroupModel := kk_http.ApiGroupModel{
+	apiGroupModel := kk_api_gen.ApiGroupModel{
 		ApiPkgName:  "kk_etcd_models",
 		HandlerName: "hUser",
 		Tag:         "user",
 		GroupUrl:    "/user/",
 	}
 	apis := []struct {
-		apiModel kk_http.ApiModel
+		apiModel kk_api_gen.ApiModel
 	}{
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "login",
 				HandlerFuncName: "Login",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "logout",
 				HandlerFuncName: "Logout",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "add user",
 				HandlerFuncName: "UserAdd",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "delete user",
 				HandlerFuncName: "UserDelete",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "get user",
 				HandlerFuncName: "GetUser",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "get my info",
 				HandlerFuncName: "MyInfo",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "list user",
 				HandlerFuncName: "UserList",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "grant role",
 				HandlerFuncName: "UserGrantRole",
 			},
@@ -67,54 +67,55 @@ func TestUser(t *testing.T) {
 
 	for _, api := range apis {
 		t.Run(api.apiModel.HandlerFuncName, func(t *testing.T) {
-			kk_http.GenerateApi(apiGroupModel, api.apiModel)
-			kk_http.GenerateDartApi(apiGroupModel, api.apiModel)
+			kk_api_gen.GenerateApi(apiGroupModel, api.apiModel)
+			kk_api_gen.GenerateDartApi(apiGroupModel, api.apiModel)
+			kk_api_gen.GenerateTypescriptApi(apiGroupModel, api.apiModel)
 		})
 	}
 }
 func TestRole(t *testing.T) {
-	apiGroupModel := kk_http.ApiGroupModel{
+	apiGroupModel := kk_api_gen.ApiGroupModel{
 		ApiPkgName:  "kk_etcd_models",
 		HandlerName: "hRole",
 		Tag:         "role",
 		GroupUrl:    "/role/",
 	}
 	apis := []struct {
-		apiModel kk_http.ApiModel
+		apiModel kk_api_gen.ApiModel
 	}{
 		//role
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "add role",
 				HandlerFuncName: "RoleAdd",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "delete role",
 				HandlerFuncName: "RoleDelete",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "list role",
 				HandlerFuncName: "RoleList",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "get role",
 				HandlerFuncName: "RoleGet",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "grant permission",
 				HandlerFuncName: "RoleGrantPermission",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "revoke permission",
 				HandlerFuncName: "RoleRevokePermission",
 			},
@@ -123,42 +124,43 @@ func TestRole(t *testing.T) {
 
 	for _, api := range apis {
 		t.Run(api.apiModel.HandlerFuncName, func(t *testing.T) {
-			kk_http.GenerateApi(apiGroupModel, api.apiModel)
-			kk_http.GenerateDartApi(apiGroupModel, api.apiModel)
+			kk_api_gen.GenerateApi(apiGroupModel, api.apiModel)
+			kk_api_gen.GenerateDartApi(apiGroupModel, api.apiModel)
+			kk_api_gen.GenerateTypescriptApi(apiGroupModel, api.apiModel)
 		})
 	}
 }
 func TestKV(t *testing.T) {
-	apiGroupModel := kk_http.ApiGroupModel{
+	apiGroupModel := kk_api_gen.ApiGroupModel{
 		ApiPkgName:  "kk_etcd_models",
 		HandlerName: "hKV",
 		Tag:         "kv",
 		GroupUrl:    "/kv/",
 	}
 	apis := []struct {
-		apiModel kk_http.ApiModel
+		apiModel kk_api_gen.ApiModel
 	}{
 		//kv
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "put kv",
 				HandlerFuncName: "KVPut",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "get kv",
 				HandlerFuncName: "KVGet",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "del kv",
 				HandlerFuncName: "KVDel",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "list kv",
 				HandlerFuncName: "KVList",
 			},
@@ -167,30 +169,31 @@ func TestKV(t *testing.T) {
 
 	for _, api := range apis {
 		t.Run(api.apiModel.HandlerFuncName, func(t *testing.T) {
-			kk_http.GenerateApi(apiGroupModel, api.apiModel)
-			kk_http.GenerateDartApi(apiGroupModel, api.apiModel)
+			kk_api_gen.GenerateApi(apiGroupModel, api.apiModel)
+			kk_api_gen.GenerateDartApi(apiGroupModel, api.apiModel)
+			kk_api_gen.GenerateTypescriptApi(apiGroupModel, api.apiModel)
 		})
 	}
 }
 func TestServer(t *testing.T) {
-	apiGroupModel := kk_http.ApiGroupModel{
+	apiGroupModel := kk_api_gen.ApiGroupModel{
 		ApiPkgName:  "kk_etcd_models",
 		HandlerName: "hServer",
 		Tag:         "server",
 		GroupUrl:    "/server/",
 	}
 	apis := []struct {
-		apiModel kk_http.ApiModel
+		apiModel kk_api_gen.ApiModel
 	}{
 		//server
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "list server",
 				HandlerFuncName: "ServerList",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "deregister server",
 				HandlerFuncName: "DeregisterServer",
 			},
@@ -199,48 +202,49 @@ func TestServer(t *testing.T) {
 
 	for _, api := range apis {
 		t.Run(api.apiModel.HandlerFuncName, func(t *testing.T) {
-			kk_http.GenerateApi(apiGroupModel, api.apiModel)
-			kk_http.GenerateDartApi(apiGroupModel, api.apiModel)
+			kk_api_gen.GenerateApi(apiGroupModel, api.apiModel)
+			kk_api_gen.GenerateDartApi(apiGroupModel, api.apiModel)
+			kk_api_gen.GenerateTypescriptApi(apiGroupModel, api.apiModel)
 		})
 	}
 }
 func TestBackup(t *testing.T) {
-	apiGroupModel := kk_http.ApiGroupModel{
+	apiGroupModel := kk_api_gen.ApiGroupModel{
 		ApiPkgName:  "kk_etcd_models",
 		HandlerName: "hBackup",
 		Tag:         "backup",
 		GroupUrl:    "/backup/",
 	}
 	apis := []struct {
-		apiModel kk_http.ApiModel
+		apiModel kk_api_gen.ApiModel
 	}{
 		//backup
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "snapshot",
 				HandlerFuncName: "Snapshot",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "snapshot restore",
 				HandlerFuncName: "SnapshotRestore",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "snapshot info",
 				HandlerFuncName: "SnapshotInfo",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "all kvs backup",
 				HandlerFuncName: "AllKVsBackup",
 			},
 		},
 		{
-			apiModel: kk_http.ApiModel{
+			apiModel: kk_api_gen.ApiModel{
 				Description:     "all kvs restore",
 				HandlerFuncName: "AllKVsRestore",
 			},
@@ -249,8 +253,9 @@ func TestBackup(t *testing.T) {
 
 	for _, api := range apis {
 		t.Run(api.apiModel.HandlerFuncName, func(t *testing.T) {
-			kk_http.GenerateApi(apiGroupModel, api.apiModel)
-			kk_http.GenerateDartApi(apiGroupModel, api.apiModel)
+			kk_api_gen.GenerateApi(apiGroupModel, api.apiModel)
+			kk_api_gen.GenerateDartApi(apiGroupModel, api.apiModel)
+			kk_api_gen.GenerateTypescriptApi(apiGroupModel, api.apiModel)
 		})
 	}
 }
--- kk_etcd_models/bind_check_gen_test.go
+++ kk_etcd_models/bind_check_gen_test.go
@@ -1,12 +1,12 @@
 package kk_etcd_models
 
 import (
-	"gitee.com/cruvie/kk_go_kit/kk_http"
+	"gitee.com/cruvie/kk_go_kit/kk_http/kk_api_gen"
 	"testing"
 )
 
 func TestBindCheckGen(t *testing.T) {
-	kk_http.BindCheckGen(
+	kk_api_gen.BindCheckGen(
 		//RoleRevokePermissionParam{},
 		ServerListParam{},
 		//DeregisterServerParam{},
